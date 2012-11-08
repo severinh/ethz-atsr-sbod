b7765d7b67ae608ecb42b62ce1f3ae1c8ab4e6ae
Severin Heiniger <severinheiniger@gmail.com>
2012-11-08 13:47:02 +0100

    Use Junit 4-style unit tests.

---
+++ src/test/java/ch/ethz/atsr/sbod/SimpleTest.java
@@ -0,0 +1,14 @@
+package ch.ethz.atsr.sbod;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleTest {
+
+	@Test
+	public void test() {
+		assertEquals(true, true);
+	}
+
+}
