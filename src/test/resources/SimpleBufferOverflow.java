f3d148ec154851e0866fdefccba651c320d8243d
Severin Heiniger <severinheiniger@gmail.com>
2012-12-02 11:33:19 +0100

    Remove obsolete test file.

--- src/test/resources/SimpleBufferOverflow.java
+++
@@ -1,9 +0,0 @@
-public class SimpleBufferOverflow {
-
-	public static void main(String[] args) {
-		int[] array = { 0, 1 };
-		int outOfBoundsElement = array[2];
-		System.out.println(outOfBoundsElement);
-	}
-
-}
