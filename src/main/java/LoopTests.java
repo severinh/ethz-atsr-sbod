ab8ee221f9c2bf1b4fb02525e27009684f95eb3f
Severin Heiniger <severinheiniger@gmail.com>
2012-12-03 23:05:03 +0100

    Add more unit tests.

--- src/main/java/LoopTests.java
+++ src/main/java/LoopTests.java
@@ -66,6 +66,78 @@ public class LoopTests extends AbstractTest {
 		assertMaybeUnsafe("testUnsafeLoopUpperLt");
 	}
 
+	public static void testSafeLoopNe() {
+		int[] array = new int[8];
+		for (int i = 7; i != -1; i--) {
+			array[i] = i;
+		}
+	}
+
+	@Test
+	public void _testSafeLoopNe() {
+		assertSafe("testSafeLoopNe");
+	}
+
+	public static void testUnsafeLoopLowerNe() {
+		int[] array = new int[8];
+		for (int i = 7; i != -2; i--) {
+			array[i] = i;
+		}
+	}
+
+	@Test
+	public void _testUnsafeLoopLowerNe() {
+		assertMaybeUnsafe("testUnsafeLoopLowerNe");
+	}
+
+	public static void testSafeLoopGt() {
+		int[] array = new int[8];
+		for (int i = 7; i > -1; i--) {
+			array[i] = i;
+		}
+	}
+
+	@Test
+	public void _testSafeLoopGt() {
+		assertSafe("testSafeLoopGt");
+	}
+
+	public static void testUnsafeLoopLowerGt() {
+		int[] array = new int[8];
+		for (int i = 7; i > -2; i--) {
+			array[i] = i;
+		}
+	}
+
+	@Test
+	public void _testUnsafeLoopLowerGt() {
+		assertMaybeUnsafe("testUnsafeLoopLowerGt");
+	}
+
+	public static void testSafeLoopGe() {
+		int[] array = new int[8];
+		for (int i = 7; i >= 0; i--) {
+			array[i] = i;
+		}
+	}
+
+	@Test
+	public void _testSafeLoopGe() {
+		assertSafe("testSafeLoopGe");
+	}
+
+	public static void testUnsafeLoopLowerGe() {
+		int[] array = new int[8];
+		for (int i = 7; i >= -1; i--) {
+			array[i] = i;
+		}
+	}
+
+	@Test
+	public void _testUnsafeLoopLowerGe() {
+		assertMaybeUnsafe("testUnsafeLoopLowerGe");
+	}
+
 	public static void testUnsafeLoopLength() {
 		int[] array = new int[8];
 		for (int i = 0; i <= array.length; i++) {
