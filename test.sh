eb34d00909e83e0dd736c1f4ee68c055fa0f84ab
Severin Heiniger <severinheiniger@gmail.com>
2012-12-07 22:13:56 +0100

    Update test.sh.

--- test.sh
+++ test.sh
@@ -8,4 +8,13 @@ export CLASSPATH=$CLASSPATH:$(pwd)/lib/junit-4.11.jar
 export CLASSPATH=$CLASSPATH:$(pwd)/lib/log4j-1.2.17.jar
 export CLASSPATH=$CLASSPATH:$(pwd)/lib/soot-2.5.0.jar
 
-java -server $MAIN_CLASS AllTests
+java -server $MAIN_CLASS AllTests \
+    AdvancedPointerAnalysis \
+    ArithmeticTests \
+    ConditionalTests \
+    ConstantTests \
+    LoopTests \
+    PointerAnalysisTests \
+    StaticFieldTests \
+    TemplateTests \
+    WideningTests
